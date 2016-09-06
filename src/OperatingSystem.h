41926b6c6895d789e39d99a589fd325c311dc4d1
Kevin M. Godby <kevin@godby.org>
2016-09-06 16:21:38 -0500

    Add Windows suspport for OS info.

--- src/OperatingSystem.h
+++ src/OperatingSystem.h
@@ -37,6 +37,9 @@
 
 // Platform-specific includes
 #if defined(OSVR_WINDOWS)
+#include <windows.h>
+#include <stdio.h>
+#include <VersionHelpers.h>
 #elif defined(OSVR_POSIX)
 #include <sys/utsname.h>
 #endif
@@ -69,6 +72,48 @@ namespace sysinfo {
             return "";
 
         return info.sysname;
+#elif defined(OSVR_WINDOWS)
+        OSVERSIONINFO version_info = {};
+        version_info.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+        GetVersionEx(&version_info);
+        const auto x = version_info.dwMajorVersion;
+        const auto y = version_info.dwMinorVersion;
+        const auto ws = (version_info.wProductType == VER_NT_WORKSTATION);
+        const auto sm_serverr2 = static_cast<bool>(GetSystemMetrics(SM_SERVERR2));
+        const auto wh_server = version_info.wSuiteMask & VER_SUITE_WH_SERVER;
+        const auto nt_workstation = (version_info.wProductType == VER_NT_WORKSTATION);
+
+        SYSTEM_INFO system_info = {};
+        GetSystemInfo(&system_info);
+        const auto arch_x64 = (system_info.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64);
+
+        if (x == 10 && y == 0) {
+            return ws ? "Windows 10" : "Windows Server 2016";
+        } else if (x == 6 && y == 3) {
+            return ws ? "Windows 8.1" : "Windows Server 2012 R2";
+        } else if (x == 6 && y == 2) {
+            return ws ? "Windows 8" : "Windows erver 2012";
+        } else if (x == 6 && y == 1) {
+            return ws ? "Windows 7" : "Windows Server 2008 R2";
+        } else if (x == 6 && y == 0) {
+            return ws ? "Windows Vista" : "Windows Server 2008";
+        } else if (x == 5 && y == 2) {
+            if (wh_server) {
+                return "Windows Home Server";
+            } else if (nt_workstation && arch_x64) {
+                return "Windows XP Professional x64 Edition";
+            } else if (sm_serverr2) {
+                return "Windows Server 2003 R2";
+            } else /* !sm_server2 */ {
+                return "Windows Server 2003";
+            }
+        } else if (x == 5 && y == 1) {
+            return "Windows XP";
+        } else if (x == 5 && y == 0) {
+            return "Windows 2000";
+        } else {
+            return "Windows";
+        }
 #else
 #error "Unsupported platform."
 #endif
@@ -84,6 +129,22 @@ namespace sysinfo {
             return "";
 
         return info.release;
+#elif defined(OSVR_WINDOWS)
+        OSVERSIONINFO version_info = {};
+        version_info.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+        GetVersionEx(&version_info);
+
+        const auto major = version_info.dwMajorVersion;
+        const auto minor = version_info.dwMinorVersion;
+        const auto build = version_info.dwBuildNumber;
+        const auto sp_str = version_info.szCSDVersion;
+
+        std::ostringstream version;
+        version << major << "." << minor << " (build " << build << ")";
+        if (sp_str) {
+            version << " " << sp_str;
+        }
+        return version.str();
 #else
 #error "Unsupported platform."
 #endif
